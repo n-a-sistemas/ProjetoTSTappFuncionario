1c1073cee9fa445fbadefcc59e4b57d0fba286cb
alicesilva <alicealmeyda72@gmail.com>
2019-09-19 17:04:20 -0300

    Início do código para o login

---
+++ app/src/main/java/com/example/tstfuncionario/Funcionario.java
@@ -0,0 +1,10 @@
+package com.example.tstfuncionario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Funcionario extends AppCompatActivity {
+
+    private String nome;
+    private String usuario;
+    private String senha;
+}
--- app/src/main/java/com/example/tstfuncionario/MainActivity.java
+++ app/src/main/java/com/example/tstfuncionario/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.tstfuncionario;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void conectaBanco(){
+        FirebaseApp.initializeApp(MainActivity.this);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+    }
+
+    private void salvarDado(View v){
+        String uuid = UUID.randomUUID().toString();
+        databaseReference
+                .child("projetotst")
+                .child(uuid)
+                .child("usuario")
+                .child()
+
     }
 }
