41e527ea71d5ce7aec465ea7449b415cfcd956ca
Me <alicealmeyda72@gmail.com>
2019-09-23 22:04:42 -0300

    Login

--- app/src/main/java/com/example/tstfuncionario/MainActivity.java
+++ app/src/main/java/com/example/tstfuncionario/MainActivity.java
@@ -1,34 +1,48 @@
 package com.example.tstfuncionario;
 
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatEditText;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.content.Intent;
 
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.common.util.SharedPreferencesUtils;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseDatabase firebaseDatabase;
     private DatabaseReference databaseReference;
-    private EditText editTextUsuario;
-    private EditText editTextSenha;
+
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editTextUsuario = findViewById(R.id.edit_text_usuario);
-        editTextSenha = findViewById(R.id.edit_text_senha);
+
+        sharedPreferences = getSharedPreferences("LOGIN", Context.MODE_PRIVATE);
+        String resultado = sharedPreferences.getString("LOGIN", "");
+
+        if (!Boolean.parseBoolean(resultado)){
+            criarLogin();
+        }
 
         conectaBanco();
     }
@@ -39,19 +53,16 @@ public class MainActivity extends AppCompatActivity {
         databaseReference = firebaseDatabase.getReference();
     }
 
-    public void salvarDado(View view){
-        String uuid = UUID.randomUUID().toString();
-        databaseReference
-                .child("projetotst")
-                .child(uuid)
-                .child("usuario")
-                .setValue(editTextUsuario.getText().toString());
+    private void criarLogin(){
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                 new AuthUI.IdpConfig.EmailBuilder().build()
+        );
 
-        databaseReference
-                .child("projetotst")
-                .child(uuid)
-                .child("senha")
-                .setValue(editTextSenha.getText().toString());
-
+        startActivityForResult(
+                AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),123
+        );
     }
 }
